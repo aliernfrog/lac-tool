214df84616fb50784c4a0fd7672ba0e0408569c0
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-03-08 20:32:05 +0300

    Removed uri permission intent

--- app/src/main/java/com/aliernfrog/LacMapTool/MainActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/MainActivity.java
@@ -12,10 +12,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Process;
 import android.os.StrictMode;
-import android.provider.DocumentsContract;
-import android.provider.Settings;
 import android.text.Html;
 import android.view.MotionEvent;
 import android.view.View;
@@ -58,10 +55,6 @@ public class MainActivity extends AppCompatActivity {
     String backupPath;
     String aBackupPath;
 
-    Uri lacTreeUri;
-    Uri lacUri;
-    int takeFlags;
-
     SharedPreferences update;
     SharedPreferences config;
 
@@ -98,22 +91,6 @@ public class MainActivity extends AppCompatActivity {
         updateText = findViewById(R.id.main_updateText);
         updateLinear = findViewById(R.id.main_updates);
         warnLinear = findViewById(R.id.main_warnings);
-
-        if (Build.VERSION.SDK_INT >= 30) {
-            devLog("android 11 detected", false);
-            lacTreeUri = DocumentsContract.buildTreeDocumentUri("com.android.externalstorage.documents", "primary:");
-            lacUri = DocumentsContract.buildDocumentUri("com.android.externalstorage.documents", "primary:");
-            takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION;
-            if (getApplicationContext().checkUriPermission(lacTreeUri, Process.myPid(), Process.myUid(), Intent.FLAG_GRANT_READ_URI_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
-                devLog("no permissions to lac data, attempting to request", false);
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-                        .putExtra(DocumentsContract.EXTRA_INITIAL_URI, lacUri)
-                        .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-                        .putExtra(Intent.EXTRA_LOCAL_ONLY, true)
-                        .addFlags(takeFlags);
-                startActivityForResult(intent, 4);
-            }
-        }
 
         if (!devMode) log.setVisibility(View.GONE);
         setListeners();
@@ -301,13 +278,6 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == 1) {
                 switchActivity(SplashActivity.class, true);
                 finish();
-            }
-        }
-        if (requestCode == 4) {
-            if (Build.VERSION.SDK_INT >= 30) {
-                int toTake = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
-                grantUriPermission(getApplicationContext().getPackageName(), data.getData(), toTake);
-                getApplicationContext().getContentResolver().takePersistableUriPermission(data.getData(), toTake);
             }
         }
     }
