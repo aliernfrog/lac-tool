da182cefa10f9c59d56697ad9b2974f7580e8931
aliernfrog <blogsimulasyon@gmail.com>
2021-03-28 13:59:33 +0300

    Remove unused imports

--- app/src/main/java/com/aliernfrog/LacMapTool/FeedbackActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/FeedbackActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.text.Html;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
--- app/src/main/java/com/aliernfrog/LacMapTool/OptionsActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/OptionsActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Switch;
