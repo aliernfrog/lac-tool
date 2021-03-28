ca77f3ee77eca69aced9805d5f9e18b22da40b01
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-03-28 13:59:33 +0300

    Remove unused imports

--- app/src/main/java/com/aliernfrog/LacMapTool/MapsOptionsActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/MapsOptionsActivity.java
@@ -17,7 +17,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
