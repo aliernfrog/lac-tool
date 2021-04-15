1b52b6caf5a6ea03703bbf9832512bfa455394a3
aliernfrog <blogsimulasyon@gmail.com>
2021-04-15 14:04:33 +0300

    Remove notes / update linear

--- app/src/main/java/com/aliernfrog/LacMapTool/MainActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/MainActivity.java
@@ -22,26 +22,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aliernfrog.LacMapTool.utils.AppUtil;
-import com.aliernfrog.LacMapTool.utils.FileUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 @SuppressLint("ClickableViewAccessibility")
 public class MainActivity extends AppCompatActivity {
-    Button missingPerms;
+    LinearLayout missingPerms;
     LinearLayout optionsLinear;
     LinearLayout redirectMaps;
     LinearLayout redirectWallpaper;
     LinearLayout redirectGallery;
     LinearLayout redirectOptions;
-    LinearLayout updateLinear;
-    TextView updateText;
-    TextView updateNotes;
-    Button updateButton;
     TextView log;
 
     String logs = "";
@@ -80,17 +72,31 @@ public class MainActivity extends AppCompatActivity {
         redirectWallpaper = findViewById(R.id.main_wallpaper_linear);
         redirectGallery = findViewById(R.id.main_gallery_linear);
         redirectOptions = findViewById(R.id.main_settings_linear);
-        updateButton = findViewById(R.id.main_updateApp);
-        updateNotes = findViewById(R.id.main_notes);
         log = findViewById(R.id.main_log);
-        updateText = findViewById(R.id.main_updateText);
-        updateLinear = findViewById(R.id.main_updates);
 
         if (!devMode) log.setVisibility(View.GONE);
-        setListeners();
         checkPerms();
         createFiles();
-        getLog();
+        setListeners();
+    }
+
+    public void checkPerms() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                hasPerms = false;
+                missingPerms.setVisibility(View.VISIBLE);
+                devLog("permission denied, attempting to request permission");
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
+            } else {
+                hasPerms = true;
+                missingPerms.setVisibility(View.GONE);
+                devLog("permissions granted");
+            }
+        } else {
+            hasPerms = true;
+            missingPerms.setVisibility(View.GONE);
+            devLog("old SDK version detected");
+        }
     }
 
     public void createFiles() {
@@ -115,7 +121,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void switchActivity(Class i, Boolean allowWithoutPerms) {
-        if (update.getBoolean("blockAccess", false)) return;
         if (!allowWithoutPerms && !hasPerms) {
             devLog("no required permissions, checking again");
             checkPerms();
@@ -130,95 +135,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void redirectURL(String url) {
-        devLog("redirecting to: "+url);
-        Intent viewIntent = new Intent("android.intent.action.VIEW", Uri.parse(url));
-        startActivity(viewIntent);
-    }
-
-    public void copyFile(String src, String dst) {
-        devLog("attempting to copy "+src+" to "+dst);
-        try {
-            FileUtil.copyFile(src, dst);
-        } catch (Exception e) {
-            e.printStackTrace();
-            devLog(e.toString());
-        }
-    }
-
     public void mkdirs(File mk) {
-        mk.mkdirs();
-        devLog("mkdirs: "+mk.getPath());
-    }
-
-    public String timeString(String frmString) {
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat frm = new SimpleDateFormat(frmString);
-        Date now = Calendar.getInstance().getTime();
-        return frm.format(now);
-    }
-
-    public void checkPerms() {
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                hasPerms = false;
-                missingPerms.setVisibility(View.VISIBLE);
-                devLog("permission denied, attempting to request permission");
-                Toast.makeText(getApplicationContext(), R.string.info_storagePerm, Toast.LENGTH_SHORT).show();
-                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 3);
-            } else {
-                hasPerms = true;
-                missingPerms.setVisibility(View.GONE);
-                devLog("permissions granted");
-            }
-        } else {
-            hasPerms = true;
-            missingPerms.setVisibility(View.GONE);
-            devLog("old SDK version detected");
-        }
-    }
-
-    public void getLog() {
-        if (update.getBoolean("update-available", false)) {
-            devLog("update found");
-            updateLinear.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.linear_blue));
-            updateNotes.setText(update.getString("update-changelog", null));
-            if (updateNotes.getText().length() < 1) updateNotes.setVisibility(View.GONE);
-            updateText.setVisibility(View.VISIBLE);
-            updateButton.setVisibility(View.VISIBLE);
-            updateButton.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (event.getAction() == MotionEvent.ACTION_UP) {
-                        redirectURL(update.getString("update-download", null));
-                    }
-                    AppUtil.handleOnPressEvent(v, event);
-                    return true;
-                }
-            });
-        } else {
-            devLog("update not found");
-            String noteFull = update.getString("notes", "");
-            if (noteFull.length() > 0) {
-                devLog("notes found");
-                if (noteFull.contains("%BUTTON%")) {
-                    updateLinear.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.linear_button));
-                    noteFull = noteFull.replace("%BUTTON%", "");
-                }
-                if (noteFull.contains("%BLUE%")) {
-                    updateLinear.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.linear_blue));
-                    noteFull = noteFull.replace("%BLUE%", "");
-                }
-                if (noteFull.contains("%RED%")) {
-                    updateLinear.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.linear_red));
-                    noteFull = noteFull.replace("%RED%", "");
-                }
-                updateNotes.setText(noteFull);
-            } else {
-                devLog("notes not found");
-                updateNotes.setVisibility(View.GONE);
-                updateLinear.setVisibility(View.GONE);
-            }
-        }
+        boolean state = mk.mkdirs();
+        devLog(mk.getPath()+" //"+state);
     }
 
     void devLog(String toLog) {
@@ -245,9 +164,6 @@ public class MainActivity extends AppCompatActivity {
         missingPerms.setOnClickListener(v -> checkPerms());
 
         optionsLinear.setOnTouchListener((v, event) -> {
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-
-            }
             AppUtil.handleOnPressEvent(v, event);
             return true;
         });
@@ -279,14 +195,6 @@ public class MainActivity extends AppCompatActivity {
         redirectOptions.setOnTouchListener((v, event) -> {
             if (event.getAction() == MotionEvent.ACTION_UP) {
                 switchActivity(OptionsActivity.class, true);
-            }
-            AppUtil.handleOnPressEvent(v, event);
-            return true;
-        });
-
-        updateLinear.setOnTouchListener((v, event) -> {
-            if (event.getAction() == MotionEvent.ACTION_UP) {
-
             }
             AppUtil.handleOnPressEvent(v, event);
             return true;
