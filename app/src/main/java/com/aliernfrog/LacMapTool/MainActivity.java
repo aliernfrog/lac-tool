038427a980d51b10348dbe0d4f414225acf0ebe7
aliernfrog <blogsimulasyon@gmail.com>
2021-04-15 14:12:01 +0300

    Remove unused imports

--- app/src/main/java/com/aliernfrog/LacMapTool/MainActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/MainActivity.java
@@ -9,17 +9,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.text.Html;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.aliernfrog.LacMapTool.utils.AppUtil;
 
