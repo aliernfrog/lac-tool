f58c9c5f1091d96810bd3fe5f69d06313a1f380a
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-04-16 11:09:30 +0300

    Remove unused imports

--- app/src/main/java/com/aliernfrog/LacMapTool/utils/WebUtil.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/utils/WebUtil.java
@@ -1,9 +1,5 @@
 package com.aliernfrog.LacMapTool.utils;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
