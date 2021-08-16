089c553f7f0e6de7ff77fe72528114149444f87e
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-08-16 15:35:29 +0300

    Check if debug mode is enabled

--- app/src/main/java/com/aliernfrog/LacMapTool/NewMapsActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/NewMapsActivity.java
@@ -1,22 +1,34 @@
 package com.aliernfrog.LacMapTool;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Process;
+import android.provider.DocumentsContract;
 import android.text.Html;
 import android.text.SpannableString;
-import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.aliernfrog.LacMapTool.utils.AppUtil;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NewMapsActivity extends AppCompatActivity {
+    FloatingActionButton saveButton;
     Button pickMap;
     TextView debugText;
+
+    SharedPreferences prefsUpdate;
+    SharedPreferences prefsConfig;
 
     Integer REQUEST_PICK_MAP = 1;
     Integer REQUEST_PICK_THUMBNAIL = 2;
@@ -24,24 +36,62 @@ public class NewMapsActivity extends AppCompatActivity {
 
     Integer uriSdkVersion = 29;
 
+    String lacPath;
+
+    Uri lacTreeUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_maps);
 
+        saveButton = findViewById(R.id.maps_save);
         pickMap = findViewById(R.id.maps_pick_pick);
         debugText = findViewById(R.id.maps_debug);
 
+        prefsUpdate = getSharedPreferences("APP_UPDATE", MODE_PRIVATE);
+        prefsConfig = getSharedPreferences("APP_CONFIG", MODE_PRIVATE);
+
+        lacPath = prefsUpdate.getString("path-maps", null);
+
+        if (prefsConfig.getBoolean("enableDebug", false)) debugText.setVisibility(View.VISIBLE);
+
         devLog("NewMapsActivity started");
 
+        checkUriPerms();
         setListeners();
     }
 
-    void pickFile(String fileType, Integer requestCode) {
+    public void pickFile(String fileType, Integer requestCode) {
         devLog("attempting to pick a file with request code: "+requestCode);
         Intent intent = new Intent(this, FilePickerActivity.class);
         intent.putExtra("FILE_TYPE", fileType);
         startActivityForResult(intent, requestCode);
+    }
+
+    @SuppressLint("NewApi")
+    void checkUriPerms() {
+        if (Build.VERSION.SDK_INT >= uriSdkVersion) {
+            saveButton.setVisibility(View.VISIBLE);
+            String treeId = lacPath.replace(Environment.getExternalStorageDirectory()+"/", "primary:");
+            Uri uri = DocumentsContract.buildDocumentUri("com.android.externalstorage.documents", treeId);
+            lacTreeUri = DocumentsContract.buildTreeDocumentUri("com.android.externalstorage.documents", treeId);
+            int takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION;
+            if (getApplicationContext().checkUriPermission(lacTreeUri, Process.myPid(), Process.myUid(), Intent.FLAG_GRANT_READ_URI_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+                devLog("no permissions to lac data, attempting to request");
+                Toast.makeText(getApplicationContext(), R.string.info_treePerm, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+                        .putExtra(DocumentsContract.EXTRA_INITIAL_URI, uri)
+                        .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+                        .putExtra(Intent.EXTRA_LOCAL_ONLY, true)
+                        .addFlags(takeFlags);
+                startActivityForResult(intent, REQUEST_URI);
+            }
+        }
+    }
+
+    void devLog(String toLog) {
+        AppUtil.devLog(toLog, debugText);
     }
 
     @Override
@@ -56,18 +106,18 @@ public class NewMapsActivity extends AppCompatActivity {
             } else {
                 devLog("no data");
             }
+        } else if (requestCode == REQUEST_URI) {
+            if (Build.VERSION.SDK_INT >= 30 && hasData) {
+                int takeFlags = Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+                grantUriPermission(getApplicationContext().getPackageName(), data.getData(), takeFlags);
+                getApplicationContext().getContentResolver().takePersistableUriPermission(data.getData(), takeFlags);
+                devLog(requestCode+": granted permissions for: "+data.getData());
+                finish();
+            } else {
+                devLog("no data");
+            }
         } else {
             devLog("result is not handled");
-        }
-    }
-
-    void devLog(String toLog) {
-        if (debugText.getVisibility() == View.VISIBLE) {
-            String tag = Thread.currentThread().getStackTrace()[3].getMethodName();
-            if (toLog.contains("Exception")) toLog = "<font color=red>"+toLog+"</font>";
-            String log = Html.toHtml(new SpannableString(debugText.getText()));
-            String full = log+"<font color=#00FFFF>["+tag+"]</font> "+toLog;
-            debugText.setText(Html.fromHtml(full));
         }
     }
 
