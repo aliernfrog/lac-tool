aab7eae204e70a6cb7e0d686e9eb5a2a401c92f6
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2021-08-19 22:41:15 +0300

    Remove unused imports

--- app/src/main/java/com/aliernfrog/LacMapTool/OptionsActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/OptionsActivity.java
@@ -1,7 +1,6 @@
 package com.aliernfrog.LacMapTool;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -10,7 +9,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
