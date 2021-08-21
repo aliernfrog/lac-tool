be086031bffc6ac4738a0b88cfa7932b097dfa8c
aliernfrog <blogsimulasyon@gmail.com>
2021-08-21 23:49:59 +0300

    Experimental options linear

--- app/src/main/java/com/aliernfrog/LacMapTool/OptionsActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/OptionsActivity.java
@@ -9,7 +9,6 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
