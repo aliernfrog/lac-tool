abf8365a6fff33fc32c50fccf4aa42b92c689bbf
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-01-15 20:03:46 +0300

    Change default updateUrl

--- app/src/main/java/com/aliernfrog/LacMapTool/utils/AppUtil.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/utils/AppUtil.java
@@ -74,7 +74,7 @@ public class AppUtil {
         SharedPreferences update = context.getSharedPreferences("APP_UPDATE", Context.MODE_PRIVATE);
         SharedPreferences config = context.getSharedPreferences("APP_CONFIG", Context.MODE_PRIVATE);
         SharedPreferences.Editor updateEdit = update.edit();
-        String updateUrl = config.getString("updateUrl", "https://aliernfrog.glitch.me/lacmaptool/update.json");
+        String updateUrl = config.getString("updateUrl", "https://raw.githubusercontent.com/aliernfrog/aliernfrog-files/master/apps/lactool/update.json");
         String rawUpdate = WebUtil.getContentFromURL(updateUrl);
         JSONObject object = new JSONObject(rawUpdate);
         updateEdit.putInt("updateLatest", object.getInt("latest"));
