88b75b2690a688d0e620a8f0e6141cf582dcb864
aliernfrog <blogsimulasyon@gmail.com>
2022-01-16 21:39:02 +0300

    Remove unused import

--- app/src/main/java/com/aliernfrog/LacMapTool/MapsActivity.java
+++ app/src/main/java/com/aliernfrog/LacMapTool/MapsActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
