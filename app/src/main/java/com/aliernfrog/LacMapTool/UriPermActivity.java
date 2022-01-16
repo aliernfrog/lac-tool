b3f60a0ce354dc0f5cfdbe0fc3d5e151d01d8f7e
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-01-16 14:54:28 +0300

    Add UriPermActivity

---
+++ app/src/main/java/com/aliernfrog/LacMapTool/UriPermActivity.java
@@ -0,0 +1,13 @@
+package com.aliernfrog.LacMapTool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UriPermActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
