968ab45279942493d9dc83358a79b2eff511ee75
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-12-01 23:22:09 +0300

    fix(ButtonShapeless): remember interaction state

--- app/src/main/java/com/aliernfrog/lactool/ui/composable/LACToolButtonShapeless.kt
+++ app/src/main/java/com/aliernfrog/lactool/ui/composable/LACToolButtonShapeless.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,7 +40,7 @@ fun LACToolButtonShapeless(
     Row(Modifier.fillMaxWidth().heightIn(44.dp)
         .clip(if (rounded) LACToolComposableShape else RectangleShape)
         .clickable(
-            interactionSource = MutableInteractionSource(),
+            interactionSource = remember { MutableInteractionSource() },
             indication = rememberRipple(color = contentColor),
             onClick = onClick
         )
