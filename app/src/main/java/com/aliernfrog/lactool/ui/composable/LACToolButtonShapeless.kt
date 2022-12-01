09c8e948b0dff7474342fb935102c2613ce260ab
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-12-01 23:00:40 +0300

    feat(ButtonShapeless): ripple color

--- app/src/main/java/com/aliernfrog/lactool/ui/composable/LACToolButtonShapeless.kt
+++ app/src/main/java/com/aliernfrog/lactool/ui/composable/LACToolButtonShapeless.kt
@@ -3,9 +3,11 @@ package com.aliernfrog.lactool.ui.composable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,14 +25,32 @@ import androidx.compose.ui.unit.sp
 import com.aliernfrog.lactool.LACToolComposableShape
 
 @Composable
-fun LACToolButtonShapeless(title: String, description: String? = null, painter: Painter? = null, rounded: Boolean = false, expanded: Boolean? = null, contentColor: Color = MaterialTheme.colorScheme.onSurface, onClick: () -> Unit) {
-    val arrowRotation = animateFloatAsState(if (expanded == true) 0f else 180f)
-    Row(Modifier.fillMaxWidth().heightIn(44.dp).clip(if (rounded) LACToolComposableShape else RectangleShape).clickable { onClick() }.padding(horizontal = 16.dp, vertical = 4.dp), verticalAlignment = Alignment.CenterVertically) {
+fun LACToolButtonShapeless(
+    title: String,
+    description: String? = null,
+    painter: Painter? = null,
+    rounded: Boolean = false,
+    expanded: Boolean? = null,
+    arrowRotation: Float = if (expanded == true) 0f else 180f,
+    contentColor: Color = MaterialTheme.colorScheme.onSurface,
+    onClick: () -> Unit
+) {
+    val animatedRotation = animateFloatAsState(arrowRotation)
+    Row(Modifier.fillMaxWidth().heightIn(44.dp)
+        .clip(if (rounded) LACToolComposableShape else RectangleShape)
+        .clickable(
+            interactionSource = MutableInteractionSource(),
+            indication = rememberRipple(color = contentColor),
+            onClick = onClick
+        )
+        .padding(horizontal = 16.dp, vertical = 4.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         if (painter != null) Image(painter, title, Modifier.padding(end = 4.dp).size(40.dp).padding(4.dp), colorFilter = ColorFilter.tint(contentColor))
         Column(Modifier.fillMaxWidth().padding(vertical = 4.dp).weight(1f)) {
             Text(text = title, color = contentColor, fontWeight = FontWeight.Bold, fontSize = 16.sp)
             if (description != null) Text(text = description, color = contentColor, fontSize = 14.sp)
         }
-        if (expanded != null) Image(Icons.Default.KeyboardArrowUp, null, modifier = Modifier.rotate(arrowRotation.value), colorFilter = ColorFilter.tint(contentColor))
+        if (expanded != null) Image(Icons.Default.KeyboardArrowUp, null, modifier = Modifier.rotate(animatedRotation.value), colorFilter = ColorFilter.tint(contentColor))
     }
 }
