6ca36a91b29c9c530375b4079a9aa393815aed21
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2022-12-19 22:35:56 +0300

    feat(screenshots): handle empty dir

--- app/src/main/java/com/aliernfrog/lactool/ui/screen/ScreenshotsScreen.kt
+++ app/src/main/java/com/aliernfrog/lactool/ui/screen/ScreenshotsScreen.kt
@@ -1,15 +1,20 @@
 package com.aliernfrog.lactool.ui.screen
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.aliernfrog.lactool.R
 import com.aliernfrog.lactool.state.ScreenshotsState
+import com.aliernfrog.lactool.ui.component.ColumnRounded
 import com.aliernfrog.lactool.ui.component.ImageButton
 
 @Composable
@@ -20,6 +25,17 @@ fun ScreenshotScreen(screenshotsState: ScreenshotsState) {
         modifier = Modifier.fillMaxSize(),
         state = screenshotsState.lazyListState
     ) {
+        item {
+            AnimatedVisibility(
+                visible = screenshotsState.importedScreenshots.value.isEmpty(),
+                enter = expandVertically() + fadeIn(),
+                exit = shrinkVertically() + fadeOut()
+            ) {
+                ColumnRounded(color = MaterialTheme.colorScheme.error) {
+                    Text(stringResource(R.string.screenshots_noScreenshots), color = MaterialTheme.colorScheme.onError, fontWeight = FontWeight.Bold)
+                }
+            }
+        }
         items(screenshotsState.importedScreenshots.value) {
             ImageButton(
                 model = it.painterModel,
