9891a49af3de82cc9436e6c9128b6d30f77f55bc
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-01-22 13:05:45 +0300

    feat: different objects for stable and preRelease

--- app/src/main/java/com/aliernfrog/lactool/state/UpdateState.kt
+++ app/src/main/java/com/aliernfrog/lactool/state/UpdateState.kt
@@ -28,10 +28,10 @@ class UpdateState(
     private val releaseUrl = config.getString(ConfigKey.KEY_APP_UPDATES_URL, ConfigKey.DEFAULT_UPDATES_URL)!!
     private val autoUpdatesEnabled = config.getBoolean(ConfigKey.KEY_APP_AUTO_UPDATES, true)
     private val currentVersionCode = GeneralUtil.getAppVersionCode(context)
+    private val isCurrentAlpha = GeneralUtil.getAppVersionName(context).contains("-alpha")
 
     var newVersionName by mutableStateOf("")
     var newVersionBody by mutableStateOf("")
-    var newVersionIsPreRelease by mutableStateOf(false)
     var newVersionDownload by mutableStateOf("")
     var updateDialogShown by mutableStateOf(false)
 
@@ -47,17 +47,16 @@ class UpdateState(
     ) {
         withContext(Dispatchers.IO) {
             try {
-                val json = JSONObject(URL(releaseUrl).readText())
+                val responseJson = JSONObject(URL(releaseUrl).readText())
+                val json = responseJson.getJSONObject(if (isCurrentAlpha) "preRelease" else "stable")
                 val latestVersionCode = json.getInt("versionCode")
                 val latestVersionName = json.getString("versionName")
                 val latestBody = json.getString("body")
-                val latestIsPreRelease = json.getBoolean("preRelease")
                 val latestDownload = json.getString("downloadUrl")
                 val isUpToDate = !ignoreVersion && latestVersionCode <= currentVersionCode
                 if (!isUpToDate) {
                     newVersionName = latestVersionName
                     newVersionBody = latestBody
-                    newVersionIsPreRelease = latestIsPreRelease
                     newVersionDownload = latestDownload
                     if (!manuallyTriggered) topToastState.showToast(
                         text = R.string.updates_updateAvailable,
