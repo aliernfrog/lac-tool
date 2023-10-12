cae1fe4e6afda6595e9373740bf4548acbde414f
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2023-10-12 21:14:15 +0300

    chore: update Gradle, Kotlin, AGP and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-val composeVersion = "1.6.0-alpha01"
-val composeCompilerVersion = "1.4.8"
+val composeMaterialVersion = "1.6.0-alpha07"
+val composeCompilerVersion = "1.5.3"
 
 android {
     namespace = "com.aliernfrog.lactool"
@@ -53,19 +53,19 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.compose.ui:ui:$composeVersion")
-    implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
-    implementation("androidx.compose.material3:material3:1.2.0-alpha03")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.navigation:navigation-compose:2.7.0-beta02")
-    implementation("io.insert-koin:koin-androidx-compose:3.4.5")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.31.4-beta")
-    implementation("com.github.aliernfrog:top-toast-compose:1.3.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.compose.ui:ui:$composeMaterialVersion")
+    implementation("androidx.compose.material:material:$composeMaterialVersion")
+    implementation("androidx.compose.material:material-icons-extended:$composeMaterialVersion")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha09")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    implementation("io.insert-koin:koin-androidx-compose:3.5.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
+    implementation("com.github.aliernfrog:top-toast-compose:1.3.4")
     implementation("com.github.aliernfrog:laclib:1.1.0")
     implementation("com.lazygeniouz:dfc:1.0.7")
     implementation("io.coil-kt:coil-compose:2.4.0")
-    implementation("com.github.jeziellago:compose-markdown:0.3.4")
+    implementation("com.github.jeziellago:compose-markdown:0.3.6")
 }
