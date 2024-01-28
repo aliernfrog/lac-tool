1c14318d2531b4efa15dc63540e40ef1853ae1a7
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-01-28 14:05:39 +0300

    chore: remove unused constructor [skip ci]

--- app/src/main/java/com/aliernfrog/lactool/impl/Progress.kt
+++ app/src/main/java/com/aliernfrog/lactool/impl/Progress.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-class ProgressState() {
+class ProgressState {
     var currentProgress by mutableStateOf<Progress?>(null)
 }
 
