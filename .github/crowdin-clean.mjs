8e6afc4958f6fd9713ec54237324e95f424875e1
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-07-28 13:54:47 +0300

    ci(crowdin): fix pulling [skip ci]

---
+++ .github/crowdin-clean.mjs
@@ -0,0 +1,14 @@
+import { readdirSync, readFileSync, rmSync } from "fs";
+
+const resPath = "./app/src/main/res";
+
+readdirSync(resPath).filter(f => f.startsWith("values-")).forEach(resFile => {
+  const folderPath = `${resPath}/${resFile}`;
+  const stringsPath = `${folderPath}/strings.xml`;
+  const content = readFileSync(stringsPath).toString();
+  const lines = content.split("\n");
+  console.log(`${stringsPath} has ${lines.length} lines`);
+  if (lines.length >= 5) return;
+  console.log(`Deleting: ${folderPath}`);
+  rmSync(folderPath, { recursive: true, force: true });
+});
