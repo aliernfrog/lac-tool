f3915a5f7e72ecef1f8694aac65c66ecc1af17c9
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-08-24 22:03:04 +0300

    crazy (please work)

--- app/src/main/java/com/aliernfrog/lactool/ui/component/CardWithActions.kt
+++ app/src/main/java/com/aliernfrog/lactool/ui/component/CardWithActions.kt
@@ -2,8 +2,11 @@ package com.aliernfrog.lactool.ui.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -25,7 +28,9 @@ fun CardWithActions(
 ) {
     Card(
         shape = AppComponentShape,
-        modifier = modifier,
+        modifier = modifier
+            .wrapContentWidth()
+            .width(IntrinsicSize.Min),
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
