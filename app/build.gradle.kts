ef2a4a562b3278051ed0c1cab5a35c5a6be756c4
alieRN <45766489+aliernfrog@users.noreply.github.com>
2024-09-04 01:09:21 +0300

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
     id("com.mikepenz.aboutlibraries.plugin")
 }
 
-val composeMaterialVersion = "1.7.0-beta06"
-val composeMaterial3Version = "1.3.0-beta05"
-val composeCompilerVersion = "1.5.14"
+val composeMaterialVersion = "1.7.0-rc01"
+val composeMaterial3Version = "1.3.0-rc01"
+val composeCompilerVersion = "1.5.15"
 val lifecycleVersion = "2.8.4"
 val shizukuVersion = "13.1.5"
 
@@ -128,8 +128,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
     implementation("androidx.activity:activity-compose:1.9.1")
-    implementation("androidx.navigation:navigation-compose:2.8.0-beta06")
-    implementation("com.mikepenz:aboutlibraries-core:11.2.2")
+    implementation("androidx.navigation:navigation-compose:2.8.0-rc01")
+    implementation("com.mikepenz:aboutlibraries-core:11.2.3")
     implementation("io.insert-koin:koin-androidx-compose:3.5.6")
     implementation("com.github.aliernfrog:top-toast-compose:2.1.0-alpha01")
     implementation("com.github.aliernfrog:laclib:1.1.0")
@@ -137,9 +137,9 @@ dependencies {
     implementation("dev.rikka.shizuku:api:$shizukuVersion")
     implementation("dev.rikka.shizuku:provider:$shizukuVersion")
     implementation("io.coil-kt:coil-compose:2.7.0")
-    implementation("com.github.jeziellago:compose-markdown:0.5.2")
-    implementation("net.engawapg.lib:zoomable:1.6.1")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation("com.github.jeziellago:compose-markdown:0.5.4")
+    implementation("net.engawapg.lib:zoomable:1.6.2")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling:$composeMaterialVersion")
     debugImplementation("androidx.compose.ui:ui-tooling-preview:$composeMaterialVersion")
