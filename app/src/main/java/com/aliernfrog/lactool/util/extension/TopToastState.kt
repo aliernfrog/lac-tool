ef63f2a7fc2399c9c4e22ea381c345dac250a8c9
aliernfrog <45766489+aliernfrog@users.noreply.github.com>
2026-01-14 20:43:36 +0300

    fix: Stack overflow caused by missing import

--- app/src/main/java/com/aliernfrog/lactool/util/extension/TopToastState.kt
+++ app/src/main/java/com/aliernfrog/lactool/util/extension/TopToastState.kt
@@ -2,6 +2,7 @@ package com.aliernfrog.lactool.util.extension
 
 import com.aliernfrog.lactool.R
 import com.aliernfrog.toptoast.state.TopToastState
+import io.github.aliernfrog.shared.util.extension.showErrorToast
 
 fun TopToastState.showErrorToast(text: Any = R.string.warning_error) {
     showErrorToast(
