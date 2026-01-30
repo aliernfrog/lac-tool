d029614178348ead69cb73d4249a4976ef84f2d9
alieRN <45766489+aliernfrog@users.noreply.github.com>
2026-01-30 20:58:02 +0300

    feat(Maps merge screen): Add elevation to toolbar

--- app/src/main/java/com/aliernfrog/lactool/ui/screen/maps/MapsMergeScreen.kt
+++ app/src/main/java/com/aliernfrog/lactool/ui/screen/maps/MapsMergeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FloatingToolbarDefaults
 import androidx.compose.material3.HorizontalFloatingToolbar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -36,6 +37,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.SubcomposeLayout
@@ -139,6 +141,10 @@ private fun MergeScreen(
                     .align(Alignment.BottomCenter)
                     .systemBarsPadding()
                     .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+                    .shadow(
+                        elevation = 6.dp,
+                        shape = FloatingToolbarDefaults.ContainerShape
+                    )
             ) {
                 @Composable
                 fun ToolbarContent(showLabelsOverride: Boolean?) {
