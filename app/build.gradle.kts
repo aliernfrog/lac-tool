7a99880b2e51412176d3489fe2f5e872d986e849
alieRN <45766489+aliernfrog@users.noreply.github.com>
2026-04-14 23:30:16 +0300

    chore: Update version to `v4.2.3`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.aliernfrog.lactool"
         minSdk = 21
         targetSdk = 36
-        versionCode = 402200
-        versionName = "4.2.2"
+        versionCode = 402300
+        versionName = "4.2.3"
         vectorDrawables { useSupportLibrary = true }
     }
 
